3cbe3ceb53c02c42ea5cf5cad0c373131af22cef
Craig Dennis <contact@zephni.com>
2018-08-13 11:47:06 +0100

    Default layers interface updated so user can't make mistakes

--- XEditor/Windows/DefaultLayers.xaml.cs
+++ XEditor/Windows/DefaultLayers.xaml.cs
@@ -12,22 +12,120 @@ namespace XEditor
     /// </summary>
     public partial class DefaultLayers : Window
     {
+        public static DefaultLayers Instance;
+
         public DefaultLayers()
         {
+            if (Instance == null)
+                Instance = this;
+
             InitializeComponent();
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
 
+            foreach(var item in Global.Preferences.Read("DefaultLayers").Split(new string[] { "|" }, StringSplitOptions.None))
+                Layers.Items.Add(item);
 
+            Layers.SelectedIndex = 0;
 
-            Layers.Text = String.Join(Environment.NewLine, Global.Preferences.Read("DefaultLayers").Split(new string[] { "|" }, StringSplitOptions.None));
-            DefaultLayer.Text = Global.Preferences.Read("DefaultSelectedLayer");
+            UpdateChanges();
         }
 
         private void UpdateDefaultLayers(object sender, RoutedEventArgs e)
         {
-            Global.Preferences.Write("DefaultLayers", String.Join("|", Layers.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None)));
+            List<string> layerStrList = new List<string>();
+            foreach (var item in Layers.Items)
+                layerStrList.Add(item.ToString());
+
+            string defaultLayersString = String.Join("|", layerStrList);
+
+            Global.Preferences.Write("DefaultLayers", defaultLayersString);
             Global.Preferences.Write("DefaultSelectedLayer", DefaultLayer.Text);
             this.Close();
+        }
+
+        public void UpdateChanges()
+        {
+            // Default layer
+            var current = DefaultLayer.Text;
+            DefaultLayer.Items.Clear();
+            foreach (var item in Layers.Items)
+            {
+                DefaultLayer.Items.Add(item.ToString());
+                if (item.ToString() == current)
+                    DefaultLayer.SelectedItem = item;
+            }
+
+            if (DefaultLayer.SelectedItem == null || DefaultLayer.SelectedItem.ToString() == "")
+                DefaultLayer.SelectedItem = DefaultLayer.Items[0];
+        }
+
+        private void ContextMenu_Add(object sender, RoutedEventArgs e)
+        {
+            if (Layers.SelectedIndex == -1)
+                return;
+
+            LayerTextInputDialogue ltid = new LayerTextInputDialogue(true, "NewLayer");
+            ltid.Show();
+        }
+        private void ContextMenu_Edit(object sender, RoutedEventArgs e)
+        {
+            if (Layers.SelectedIndex == -1)
+                return;
+
+            LayerTextInputDialogue ltid = new LayerTextInputDialogue(false, Layers.SelectedItem.ToString());
+            ltid.Show();
+        }
+        private void ContextMenu_Delete(object sender, RoutedEventArgs e)
+        {
+            if (Layers.SelectedIndex == -1)
+                return;
+
+            if (Layers.Items.Count == 1)
+            {
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBox.Show("There must be at least one layer", "Can't do that!", button);
+                return;
+            }
+
+            Layers.Items.Remove(Layers.SelectedItem);
+            UpdateChanges();
+        }
+        private void ContextMenu_MoveUp(object sender, RoutedEventArgs e)
+        {
+            MoveItem(-1);
+        }
+
+        private void ContextMenu_MoveDown(object sender, RoutedEventArgs e)
+        {
+            MoveItem(1);
+        }
+
+        public void MoveItem(int direction)
+        {
+            // Checking selected item
+            if (Layers.SelectedItem == null || Layers.SelectedIndex < 0)
+                return; // No selected item - nothing to do
+
+            // Calculate new index using move direction
+            int newIndex = Layers.SelectedIndex + direction;
+
+            // Checking bounds of the range
+            if (newIndex < 0 || newIndex >= Layers.Items.Count)
+                return; // Index out of range - nothing to do
+
+            object selected = Layers.SelectedItem;
+
+            // Removing removable element
+            Layers.Items.Remove(selected);
+            // Insert it in new position
+            Layers.Items.Insert(newIndex, selected);
+            // Restore selection
+            Layers.SelectedIndex = newIndex;
+        }
+
+        private void Layers_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Layers.UnselectAll();
         }
     }
 }
---
+++ XEditor/Windows/LayerTextInputDialogue.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace XEditor
+{
+    /// <summary>
+    /// Interaction logic for LayerTextInputDialogue.xaml
+    /// </summary>
+    public partial class LayerTextInputDialogue : Window
+    {
+        private string LayerName
+        {
+            get { return LayerText.Text; }
+            set { LayerText.Text = value; }
+        }
+
+        private string InitialLayerName;
+
+        private bool CreatingNew;
+
+        public LayerTextInputDialogue(bool creatingNew, string defaultLayerName)
+        {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            LayerName = defaultLayerName;
+            InitialLayerName = LayerName;
+            CreatingNew = creatingNew;
+
+            if (!CreatingNew)
+                this.Title = "Editing layer '" + LayerName + "'";
+            else
+                this.Title = "Creating new layer";
+
+            LayerText.Focus();
+            LayerText.SelectAll();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (DefaultLayers.Instance.Layers.Items.Contains(LayerName) && InitialLayerName != LayerName)
+            {
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBox.Show("A layer with this name already exists", "Can't do that!", button);
+                return;
+            }
+
+            if (CreatingNew)
+            {
+                if (DefaultLayers.Instance.Layers.SelectedIndex > DefaultLayers.Instance.Layers.Items.Count)
+                    DefaultLayers.Instance.Layers.SelectedIndex = DefaultLayers.Instance.Layers.Items.Count;
+
+                DefaultLayers.Instance.Layers.Items.Insert(DefaultLayers.Instance.Layers.SelectedIndex+1, LayerName);
+                DefaultLayers.Instance.UpdateChanges();
+            }
+            else if(!CreatingNew)
+            {
+                var index = DefaultLayers.Instance.Layers.SelectedIndex;
+                DefaultLayers.Instance.Layers.Items.RemoveAt(index);
+                DefaultLayers.Instance.Layers.Items.Insert(index, LayerName);
+                DefaultLayers.Instance.UpdateChanges();
+                DefaultLayers.Instance.DefaultLayer.SelectedIndex = index;
+            }
+
+            this.Close();
+        }
+    }
+}
